d3b670b5facb31a1cc9b8f9ba190d05f86824e7c
CarlosEduardo113 <ce219560@gmail.com>
2025-05-17 16:40:41 -0300

    Finalizar

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿ Console.WriteLine("O FLUXOGRAMA DA ENGENHARIA\n");
+
+        Console.Write("Está se movendo? (s/n): ");
+        string movendo = Console.ReadLine().Trim().ToLower();
+
+        if (movendo == "s")
+        {
+            Console.Write("Deveria? (s/n): ");
+            string deveria = Console.ReadLine().Trim().ToLower();
+
+            if (deveria == "s")
+            {
+                Console.WriteLine("ÓTIMO");
+            }
+            else if (deveria == "n")
+            {
+                Console.WriteLine("Use fita adesiva (silver tape).");
+            }
+            else
+            {
+                Console.WriteLine("Resposta inválida.");
+            }
+        }
+        else if (movendo == "n")
+        {
+            Console.Write("Deveria? (s/n): ");
+            string deveria = Console.ReadLine().Trim().ToLower();
+
+            if (deveria == "n")
+            {
+                Console.WriteLine("ÓTIMO");
+            }
+            else if (deveria == "s")
+            {
+                Console.WriteLine("Use WD-40.");
+            }
+            else
+            {
+                Console.WriteLine("Resposta inválida.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Resposta inválida.");
+        }
